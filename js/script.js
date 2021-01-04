d0bcb527e284031951ed634c3d4ef971c42cd15d
Berserker <luanbraga.lb@gmail.com>
2021-01-03 22:29:47 -0300

    substitui o checkbox hack

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
-let botaoMagico = document.querySelector("input[name=checkbox]");
+console.log('hello world');
+
+let botaoMagico = document.querySelector("#botaoMagico");
+let botaoMagicoFecha = document.querySelector("#botaoMagicoFecha");
 let imagemCapa = document.querySelector(".imagemCapa");
 
-alteraCapa(imagemCapa);
+window.onload = function(){
 
-function alteraCapa (capa) {
-    capa.setAttribute("style","background-color: black")
+    escondeBotao(botaoMagicoFecha);
+
+    botaoMagico.addEventListener("click", function () {
+
+        minimizaCapa(imagemCapa);
+        escondeBotao(botaoMagico);
+        mostraBotao(botaoMagicoFecha);
+    } );
+
+    botaoMagicoFecha.addEventListener("click", function () {
+
+        maximizaCapa(imagemCapa);
+        escondeBotao(botaoMagicoFecha);
+        mostraBotao(botaoMagico);
+
+    } );
+
+    function minimizaCapa(capa) {
+
+        capa.style.backgroundColor = 'rgb(77, 76, 161)';
+    }
+
+    function maximizaCapa(capa) {
+
+        capa.style.backgroundColor = 'rgb(0, 0, 0)';
+    }
+
+    function escondeBotao(botao) {
+        botao.style.display = 'none';
+    }
+
+    function mostraBotao(botao) {
+        botao.style.display = 'block';
+    }
 }
